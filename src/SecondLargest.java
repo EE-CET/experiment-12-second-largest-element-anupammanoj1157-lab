300ae5d65c3e34318adccf43aedc6c2f9a03229b
Anupam Manoj <anupammanoj1157@gmail.com>
2026-04-05 21:47:28 +0530

    Completed SecondLargest

--- src/SecondLargest.java
+++ src/SecondLargest.java
@@ -1,33 +1,30 @@
 import java.util.Scanner;
 
 public class SecondLargest {
+
         public static void main(String[] args) {
-                Scanner sc = new Scanner(System.in);
-
-                if (!sc.hasNextInt())
-                        return;
-                int n = sc.nextInt();
-
-                int max1 = Integer.MIN_VALUE;
-                int max2 = Integer.MIN_VALUE;
-
-                for (int i = 0; i < n; i++) {
+                try (Scanner sc = new Scanner(System.in)) {
                         if (!sc.hasNextInt())
-                                break;
-                        int current = sc.nextInt();
+                                return;
+                        int n = sc.nextInt();
+                        int[] arr = new int[n];
 
-                        if (current > max1) {
-                                max2 = max1;
-                                max1 = current;
+                        int max1 = Integer.MIN_VALUE;
+                        int max2 = Integer.MIN_VALUE;
+
+                        for (int i = 0; i < n; i++) {
+                                if (sc.hasNextInt()) {
+                                        arr[i] = sc.nextInt();
+                                        if (arr[i] > max1) {
+                                                max2 = max1;
+                                                max1 = arr[i];
+                                        } else if (arr[i] > max2) {
+                                                max2 = arr[i];
+                                        }
+                                }
                         }
 
-                        else if (current > max2 && current < max1) {
-                                max2 = current;
-                        }
+                        System.out.println(max2);
                 }
-
-                System.out.println(max2);
-
-                sc.close();
         }
 }
